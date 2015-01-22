7f1e645e3d2fc32552bb9ccaf918687f32dfffb2
Tyler Cecchi <thcecchi@gmail.com>
2015-01-22 18:13:57 -0500

    second commit, complete layout and 3 jquery events

--- main.js
+++ main.js
@@ -1,16 +1,50 @@
 $(document).ready(function(){
 
+// click nav highlights
   $("a").click(function (event){
+    event.preventDefault();
 
     $(this).closest("li").siblings().children().removeClass("active");
     $(this).addClass("active");
 
-    var scrollSection = "." + $(this).attr('rel');
-
-      $(this).scrollTo(scrollSection);
+// click scroll to element
+    var scrSection = "." + $(this).attr('rel');
+    $("html, body").animate({ scrollTop: $(scrSection).offset().top}, 1000);
 
   });
 
+// scroll highlight nav position
+  $(window).scroll(function() {
 
+    var scrollPos = $(window).scrollTop();
+
+    if (scrollPos >= 200) {
+      $("nav a").eq(0).addClass("active");
+      $("nav a").eq(1).removeClass("active");
+    }
+
+    if (scrollPos >= 500) {
+      $("nav a").eq(1).addClass("active");
+      $("nav a").eq(0).removeClass("active");
+      $("nav a").eq(2).removeClass("active");
+    }
+
+    if (scrollPos >= 1500) {
+      $("nav a").eq(2).addClass("active");
+      $("nav a").eq(1).removeClass("active");
+      $("nav a").eq(3).removeClass("active");
+    }
+
+    if (scrollPos >= 2500) {
+      $("nav a").eq(3).addClass("active");
+      $("nav a").eq(2).removeClass("active");
+      $("nav a").eq(4).removeClass("active");
+    }
+
+    if (scrollPos >= 3500) {
+      $("nav a").eq(4).addClass("active");
+      $("nav a").eq(3).removeClass("active");
+    }
+  });
 
 });
